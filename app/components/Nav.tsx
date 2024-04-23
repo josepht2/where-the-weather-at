57cde7efc38492e61bd5d01a4f49244cb726b421
Joseph Triolo <joseph.triolo2@gmail.com>
2024-04-23 07:31:43 -0400

    Refactor Nav component

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,30 +3,29 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-// import styles from "../styles/layout.module.css"
+const ACTIVE_STYLE = 'underline underline-offset-4'
+const INACTIVE_STYLE = 'hover:underline underline-offset-4'
 
 export const Nav = () => {
   const pathname = usePathname()
 
   return (
-    <div className='flex font-sans'>
-      <div className='text-lg font-bold'>
+    <div className='flex items-center h-14 bg-white border-b border-slate-200 space-x-4'>
+      <div className='ml-5 text-2xl font-bold'>
         Where The Weather At
       </div>
       <Link
-        // className={`${styles.link} ${pathname === "/" ? styles.active : ""}`}
+        className={pathname === '/' ? ACTIVE_STYLE : INACTIVE_STYLE}
         href="/"
       >
         Home
       </Link>
-      {/* <Link
-        className={`${styles.link} ${
-          pathname === "/verify" ? styles.active : ""
-        }`}
-        href="/verify"
+      <Link
+        className={pathname === '/search-history' ? ACTIVE_STYLE : INACTIVE_STYLE}
+        href="/search-history"
       >
-        Verify
-      </Link> */}
+        Search History
+      </Link>
     </div>
   )
 }
